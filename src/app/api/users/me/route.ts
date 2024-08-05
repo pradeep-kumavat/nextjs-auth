6f2ede789492ad1802079118ee1bfcd459b1ef6e
pradeep-kumavat <pradeepkumavat108@gmail.com>
2024-08-05 20:00:49 +0530

    final commit

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -1,20 +1,22 @@
-import {connect} from "@/dbConfig/dbConfig";
-import User from "@/models/userModel";
-import { NextRequest, NextResponse } from "next/server";
 import { getDataFromToken } from "@/helpers/getDataFromToken";
 
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel";
+import { connect } from "@/dbConfig/dbConfig";
 
-connect()
+connect();
 
-export async function POST(request: NextRequest){
-    //extract data from token
-    const userId = await getDataFromToken(request)
-    const user = await User.findOne({_id:userId}).select("-password -username")
-    //check if there is no user
-    
-    return NextResponse.json({
-        message:"User found",
-        data:user
-    })
-    
+export async function GET(request:NextRequest){
+
+    try {
+        const userId = await getDataFromToken(request);
+        const user = await User.findOne({_id: userId}).select("-password");
+        return NextResponse.json({
+            message: "User found",
+            data: user
+        })
+    } catch (error:any) {
+        return NextResponse.json({error: error.message}, {status: 400});
+    }
+
 }
